e38e0aec8edf4cb2083a19c17f9a1505e6f820c1
Joekovich <Joelarmani4790@gmail.com>
2024-03-04 14:22:13 +0100

    Add project files.

---
+++ Pay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningAbstraction_in_C
+{
+    public abstract class Pay
+    {
+        protected int _basicPay = 50000; 
+        protected int _houseRent = 750000;
+        protected int _payRate = 5;
+        protected int _netPay;
+
+        public abstract int GradeOne { get; }
+        public abstract int GradeTwo { get; }
+    }
+
+    public class PayBill : Pay
+    {
+        public override int GradeOne 
+        
+        {
+            get
+            {
+                return _netPay = _basicPay +  _houseRent;
+            }
+        }
+
+        public override int GradeTwo
+        {
+            get
+            {
+                return _netPay = _basicPay + _basicPay / _houseRent * _payRate;
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("The output Don was able to pay for Gradeone is : {0},", GradeOne);
+            Console.WriteLine("The output Don was able to pay for Gradetwo is : {0},", GradeTwo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using LearningAbstraction_in_C;
+
+namespace LearningAbsractionInCSharp
+
+{
+    public abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learning Abstracting in C#****");
+            PayBill payBill = new PayBill();
+            payBill.Display();
+        }
+    }
+}
